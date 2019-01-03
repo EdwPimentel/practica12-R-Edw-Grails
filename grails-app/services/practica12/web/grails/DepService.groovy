3a279d53f106f91abf51ee51930203381a3bdde8
EDWARD WILFREDO PIMENTEL GUZMÁN <20140909@ce.pucmm.edu.do>
2019-01-03 02:10:56 -0400

    Some services

---
+++ grails-app/services/practica12/web/grails/DepService.groovy
@@ -0,0 +1,18 @@
+package practica12.web.grails
+
+import grails.gorm.services.Service
+
+@Service(Dep)
+interface DepService {
+
+    Dep get(Serializable id)
+
+    List<Dep> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    Dep save(Dep departamento)
+
+}
